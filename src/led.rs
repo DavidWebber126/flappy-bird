6967bb860f270109516cf2cc0b1f301475bb07cd
DavidWebber126 <davidwebber126@gmail.com>
2024-01-29 21:52:12 -0600

    Working PWM Speaker, single frequency

--- src/led.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug)]
-pub enum Pipe {
-    Low,
-    MidLow,
-    HighLow,
-    High,
-}
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,25 @@ use embedded_hal::{digital::v2::InputPin, blocking::delay::DelayMs};
 //use rtt_target::{rtt_init_print, rprintln};
 //use panic_rtt_target as _;
 use panic_halt as _;
+use defmt_rtt as _;
+
+use core::cell::RefCell;
+use cortex_m::interrupt::Mutex;
 
 use microbit::{
+    //Peripherals,
     display::blocking::Display,
-    hal::{Timer, Rng},
+    hal::{
+        Timer,
+        Rng,
+        clocks::Clocks,
+        gpio,
+        prelude::OutputPin,
+        pwm,
+        rtc::{Rtc, RtcInterrupt},
+        time::Hertz,
+    },
+    pac::{self, interrupt},
 };
 
 #[derive(Debug)]
@@ -20,11 +35,16 @@ pub enum Pipe {
     High,
 }
 
+static RTC: Mutex<RefCell<Option<Rtc<pac::RTC0>>>> = Mutex::new(RefCell::new(None));
+static SPEAKER: Mutex<RefCell<Option<pwm::Pwm<pac::PWM0>>>> = Mutex::new(RefCell::new(None));
+static SPEAKER_OFF: Mutex<RefCell<bool>> = Mutex::new(RefCell::new(false));
+
 #[entry]
 fn main() -> ! {
     //rtt_init_print!();
 
-    let board = microbit::Board::take().unwrap();
+    let mut board = microbit::Board::take().unwrap();
+    //let peripherals = Peripherals::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
     let mut display = Display::new(board.display_pins);
 
@@ -34,7 +54,58 @@ fn main() -> ! {
     let mut rng = Rng::new(board.RNG);
     
     let mut bird_coord = (2, 1);
+
     let mut game_over = false;
+
+    cortex_m::interrupt::free(move |cs| {
+        // NB: The LF CLK pin is used by the speaker
+        let _clocks = Clocks::new(board.CLOCK)
+            .enable_ext_hfosc()
+            .set_lfclk_src_synth()
+            .start_lfclk();
+
+        let mut rtc = Rtc::new(board.RTC0, 511).unwrap();
+        rtc.enable_counter();
+        rtc.enable_interrupt(RtcInterrupt::Tick, Some(&mut board.NVIC));
+        rtc.enable_event(RtcInterrupt::Tick);
+
+        *RTC.borrow(cs).borrow_mut() = Some(rtc);
+
+        let mut speaker_pin = board.speaker_pin.into_push_pull_output(gpio::Level::High);
+        let _ = speaker_pin.set_low();
+
+        // Use the PWM peripheral to generate a waveform for the speaker
+        let speaker = pwm::Pwm::new(board.PWM0);
+        speaker
+            // output the waveform on the speaker pin
+            .set_output_pin(pwm::Channel::C0, speaker_pin.degrade())
+            // Use prescale by 16 to achive darker sounds
+            .set_prescaler(pwm::Prescaler::Div128)
+            // Initial frequency
+            .set_period(Hertz(1u32))
+            // Configure for up and down counter mode
+            .set_counter_mode(pwm::CounterMode::UpAndDown)
+            // Set maximum duty cycle
+            .set_max_duty(32767)
+            // enable PWM
+            .enable();
+
+        speaker
+            .set_seq_refresh(pwm::Seq::Seq0, 0)
+            .set_seq_end_delay(pwm::Seq::Seq0, 0);
+
+        // Configure 50% duty cycle
+        let max_duty = speaker.max_duty();
+        speaker.set_duty_on_common(max_duty / 2);
+
+        *SPEAKER.borrow(cs).borrow_mut() = Some(speaker);
+
+        // Configure RTC interrupt
+        unsafe {
+            pac::NVIC::unmask(pac::Interrupt::RTC0);
+        }
+        pac::NVIC::unpend(pac::Interrupt::RTC0);
+    });
     
     //rprintln!("Entering main loop.");
     loop {
@@ -124,9 +195,10 @@ fn main() -> ! {
                 //timer.delay_ms(250 as u16);
 
             } else {
-
                 match bird_coord.0 {
-                    4 => game_over = true,
+                    4 => {
+                        game_over = true;
+                    },
                     _ => bird_coord.0 = bird_coord.0 + 1,
                 }
                 
@@ -162,5 +234,35 @@ fn main() -> ! {
         }
 
         //rprintln!("Game is over")
+        cortex_m::interrupt::free(move |cs| {
+            *SPEAKER_OFF.borrow(cs).borrow_mut() = true;
+        });
     }
+}
+
+// RTC interrupt, exectued for each RTC tick
+#[interrupt]
+fn RTC0() {
+    /* Enter critical section */
+    cortex_m::interrupt::free(|cs| {
+        /* Borrow devices */
+        if let (Some(speaker), Some(rtc)) = (
+            SPEAKER.borrow(cs).borrow().as_ref(),
+            RTC.borrow(cs).borrow().as_ref(),
+        ) {
+            speaker.set_period(Hertz(2));
+
+            // Restart the PWM at 50% duty cycle
+            let max_duty = speaker.max_duty();
+            speaker.set_duty_on_common(max_duty / 4);
+
+            if SPEAKER_OFF.borrow(cs).clone().into_inner() {
+                speaker.disable();
+                rtc.disable_counter();
+            }
+
+            // Clear the RTC interrupt
+            rtc.reset_event(RtcInterrupt::Tick);
+        }
+    });
 }
